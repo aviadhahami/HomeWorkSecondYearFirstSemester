6ebb499def310601df15e7660478d7053eedfff6
aviadhahami <aviadhahami@gmail.com>
2014-10-30 22:02:38 +0200

    Finalized Ex01 in Functional

--- Functional Prgrmng/Ex01/ex1-302188347.rkt
+++ Functional Prgrmng/Ex01/ex1-302188347.rkt
@@ -1,10 +1,8 @@
-(require racket/trace)
-
 ;302188347
 
-;PART1 - Prefix and Infix
+;Part 1 – Prefix and Infix (24 points)
 ;A:
-;I. ( ( 300  + 11 ) + ( 8 * 9))
+;I. ( ( 300 + 11 ) + ( 8 * 9))
 ;II. ( ( ( 1 + 2) * 8) + ( 33 / 11))
 ;III. ( ( ( ( ( ( ( 7 * 6) * 8) * 5) * 4) *  3) * 2) * 1)
 
@@ -13,34 +11,42 @@
 ;II. (* (+ (+ 8 4) 5) (/ (+ 9 7) (* (* 8 7) 6)))
 ;III. (- 2 (/ (+ 4 (* 8 9)) 4))
 
-;PART 2
 
+;Part 2 – Simple Scheme (26 points)
+;A:
+;Checking for Odd number
 (define (isOdd x)
   ;condition is if the remain does not equal zero
   (cond  ((not (= (modulo x 2) 0)) #t)
          (else #f)
          )
   )
+;Checking for positive number
 (define (isPositive x)
   (if (> x 0) #t #f)
   )
-
+;Checking if number is positive AND odd
 (define (positiveOdd x)
   (if (and (isOdd x) (isPositive x)) 'yes 'no)
   )
 
+
+;B. (13 points)
 (define (circle-area r)
-  (* pi (expt r r))
+  (* pi (expt r 2))
   )
 
-;PART 3 - SIMPLE RECURSION
+
+;Part 3 – Simple Recursion (25 points)
 (define (someSequence n)
   (if (= n 1)
       1
       (+ (expt (* n 2 ) n) (someSequence (- n 1)))
       ))
 
-;PART 4 - LESS SIMPLE RECURSION -- FIBONACI
+
+
+;Part 4 – Less Simple Recursion (25 points)
 
 ;Printing Function
 (define (printer unkn )
@@ -48,7 +54,7 @@
     (display (+ unkn 0))
     (newline)
     ))
-
+;main func matching interface demands
 (define (fibo n)
   (begin 
     (printer 1)
@@ -56,7 +62,7 @@
     (printer n)
     )
   )
-
+;Fibo-side-kick is ment to be the actual recursion
 (define (fibo-side-kick a b counter)
   (if (= counter 0)
       b
