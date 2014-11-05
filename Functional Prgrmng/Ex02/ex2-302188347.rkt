45e28527e5b58aa496e37210af9b8c5618560432
aviadhahami <aviadhahami@gmail.com>
2014-11-05 14:52:13 +0200

    added wingman

--- Functional Prgrmng/Ex02/ex2-302188347.rkt
+++ Functional Prgrmng/Ex02/ex2-302188347.rkt
@@ -1,11 +1,23 @@
-(define (carsDiver ml)
-  (if (null? ml)
-      ;lists is empty
-      '()
-      ;lists is not empty
-      (car ml)
-      ))
+(define (carsWingman ml)
+  (if (list? ml)
+      ;it is a list ! lets roll in
+      (begin
+       (carsWingman (car ml))
+       (if (null? (cdr ml))
+           ;we do none - its empty
+           (carsWingman (cdr ml)))
+        
+       )
+      ;not a list -> to print
+      (+ ml)
+      )
+  )
 
 (define (cars lists)
-  ()
+  (cond 
+    ;if no lists are in
+    ((null? lists)
+     '())
+    (else (carsWingman lists))
+    )
   )
