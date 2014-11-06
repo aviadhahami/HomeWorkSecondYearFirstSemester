9ae501fbc113a617cd59a8c28a7a9a523a1e77ee
aviadhahami <aviadhahami@gmail.com>
2014-11-06 10:33:14 +0200

    workin on Q#1

--- Functional Prgrmng/Ex02/ex2-302188347.rkt
+++ Functional Prgrmng/Ex02/ex2-302188347.rkt
@@ -1,23 +1,20 @@
-(define (carsWingman ml)
-  (if (list? ml)
-      ;it is a list ! lets roll in
-      (begin
-       (carsWingman (car ml))
-       (if (null? (cdr ml))
-           ;we do none - its empty
-           (carsWingman (cdr ml)))
-        
-       )
-      ;not a list -> to print
-      (+ ml)
-      )
+;ID: 302188347
+
+(define (carsFirstItemPrinter ml)
+  (list-ref ml 0)
   )
 
+(define (carsListHandler ml)
+  
+  ;im too drunk... we should go 2 levels inside and print...thats it
+  ;if the next item next item isnt a lost drop it
+  )
+)
 (define (cars lists)
   (cond 
     ;if no lists are in
     ((null? lists)
      '())
-    (else (carsWingman lists))
+    (else (list (carsListHandler lists)))
     )
   )
