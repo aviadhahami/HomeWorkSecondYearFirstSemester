7888001ec1ba5e88ce21c0bd65c3a2fd3c707b3b
aviadhahami <aviadhahami@gmail.com>
2014-11-06 15:11:14 +0200

    Recursion works!
    now need to print it .... :\

--- Functional Prgrmng/Ex02/ex2-302188347.rkt
+++ Functional Prgrmng/Ex02/ex2-302188347.rkt
@@ -1,15 +1,24 @@
 ;ID: 302188347
+(require racket/trace)
 
 (define (carsFirstItemPrinter ml)
-  (list-ref ml 0)
+  (list (list-ref ml 0))
   )
 
 (define (carsListHandler ml)
   
   ;im too drunk... we should go 2 levels inside and print...thats it
-  ;if the next item next item isnt a lost drop it
-  )
-)
+  ;if the next item next item isnt a list drop it
+  
+  (if (not (null? (cdr ml)))
+      (begin
+        (carsFirstItemPrinter (car ml))
+        (carsListHandler (cdr ml)))
+      '()
+      ));there exists another list
+
+
+
 (define (cars lists)
   (cond 
     ;if no lists are in
@@ -18,3 +27,4 @@
     (else (list (carsListHandler lists)))
     )
   )
+(trace carsListHandler)
