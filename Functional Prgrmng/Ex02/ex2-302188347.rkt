9ab525fd5109f66410e559d1dd47fc8c10601129
aviadhahami <aviadhahami@gmail.com>
2014-11-06 15:47:13 +0200

    added comments FLP->Ex02->Q#1

--- Functional Prgrmng/Ex02/ex2-302188347.rkt
+++ Functional Prgrmng/Ex02/ex2-302188347.rkt
@@ -1,26 +1,26 @@
 ;ID: 302188347
 (require racket/trace)
-
+;carsFirstItemPrinter purpose is just to be pretty
 (define (carsFirstItemPrinter ml)
   (list-ref ml 0)
   )
 
 (define (carsListHandler ml)
-  
   ;im too drunk... we should go 2 levels inside and print...thats it
   ;if the next item next item isnt a list drop it
-  
   (if (not (null? (cdr ml)))
+      ;Case its CDR is not null (more lists exist up ahead)
       (cons
        (carsFirstItemPrinter (car ml))
        (carsListHandler (cdr ml))
        )
+      ;Case its CDR is null -> we cons the last item with null pointer '()
       (cons (carsFirstItemPrinter (car ml)) '())
       )
   )
 
-
-
+;The function (cars lists) receives a list of lists!
+;I'm assuming proper input, there for exeption will be thrown for a list such as '(1 2 3)
 (define (cars lists)
   (cond 
     ;if no lists are in
@@ -28,5 +28,3 @@
      '())
     (else  (carsListHandler lists)))
   )
-
-(trace carsListHandler)
