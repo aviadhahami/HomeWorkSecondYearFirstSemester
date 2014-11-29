5624a2e6f102456ba588bbfd19f1f4a0e30df45b
aviadhahami <aviadhahami@gmail.com>
2014-11-29 21:26:27 +0200

    working on the last ip macro

--- Functional Prgrmng/Ex04/Ex4-302188347.rkt
+++ Functional Prgrmng/Ex04/Ex4-302188347.rkt
@@ -14,7 +14,6 @@
 ;Part 2 – Not-so-simple Macro (60 points)
 ;test-ip assumes correct input
 
-
 (define test-ip 
   (λ (ip prefix)
     (if (not(null? prefix))
@@ -29,11 +28,40 @@
 ;GGWP
 (define make-ip-filter
   (λ (prefix)
-    (λ (lst)
+    (λ 
+        (lst)
       (filter pair? (map (λ (x) (if (test-ip x prefix) x )) lst)))))
 
 
 
+(defmacro my-cond (ip . cases)
+  (define (expandCase case)
+    `(if ,(test-ip (car case)) ,(cadr case)))
+  (define (expandCases cases)
+    (if (null? cases)
+        '(void)
+        (if (eq? (caar cases) default)
+            (cadar cases)
+            (let ((current (expandCase (car cases)))
+                  (next (expandCases (cdr cases))))
+              (append current (list next))))))
+  (expandCases (cons ip cases)))
+
+
+
+;Part 3 – Boolean Logic (20 points)
+
+
+
+
+(let ((my-ip '(194 90 181 27)))
+  (my-cond my-ip
+             ((129 117) 'bezeq-international)
+             ((194 90) 'netvision)
+             ((85 44 2) 'zahav-012)
+             ((37 142 198) 'hot-net)
+             (default (display “Unknown IP address”))))
+
+
 ;TRACERS
-(trace make-ip-filter)
-(trace test-ip)
+(trace switch-ip)
