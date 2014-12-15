aca3d817185a5361c4c5aa3266a3a8f111452cd9
Aviad Hahami <aviadhahami@gmail.com>
2014-12-15 19:06:25 +0200

    finished C need to fix B
    had data structure misunderstanding

--- Functional Prgrmng/Ex05&Ex06/Ex06-302188347.rkt
+++ Functional Prgrmng/Ex05&Ex06/Ex06-302188347.rkt
@@ -4,24 +4,51 @@
 
 
 
+
+;a bit of easy-to-read syntax
+(define !
+  (λ (arg)
+    (not arg)))
+
 ;multidict getter as demanded
 (define multidict-get 
   (λ (key dict)
-    (let ((res (mdgtr dict key ())))
+    (let ((res (MDgtr-TR dict key ())))
       (if (empty? res)
           #f
           res))))
-;mdgr is a tail recursive getter for the 
-(define mdgtr
+
+;mdgr is a tail recursive helper for the getter "multidict-get" 
+(define MDgtr-TR
   (λ (dict key out)
-    (if (null? (cdr dict))
+    (if (null? dict)
         out
         (if (eq? (caar dict) key)
-            (mdgtr (cdr dict) key (append out (cdar dict)))
-            (mdgtr (cdr dict) key out)))))
+            (MDgtr-TR (cdr dict) key (append out (cdar dict)))
+            (MDgtr-TR (cdr dict) key out)))))
+
+
+;multidict-remove --> API meeting function
+(define multidict-remove
+  (λ (key dict)
+    (MTrm-TR dict () key )))
+
+
+;MTrm-TR(multidict-remove Tail Recursion) is a helper, nothing special here
+(define MTrm-TR
+  (λ (dict _dict key)
+    (if (null? dict)
+        _dict
+        (if (!(eq? (caar dict) key)) ;if we don't match the key we append to new dir...reverse logic
+            (MTrm-TR (cdr dict) (append _dict (list (car dict))) key)
+            (MTrm-TR (cdr dict) _dict key)))))
 
 
 
 
-(define sample-dict '((a 1)(a 2)(b 3)(c 4)(c 5)(c 6)))
-(trace mdgtr)
+;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+;REMOVE ME !!!
+;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+(define sample-dict '((a 1 2)(b 3)(c 4 5 6)))
+(trace MTrm-TR)
+;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
