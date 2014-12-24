77eabe98ff424f64a55b747d7dfd600342cbe73c
Aviad Hahami <aviadhahami@gmail.com>
2014-12-24 13:29:20 +0200

    going on #6

--- Functional Prgrmng/Ex07/ex7-302188347.rkt
+++ Functional Prgrmng/Ex07/ex7-302188347.rkt
@@ -226,67 +226,74 @@
 
 (define evaluate 
   (λ (exp ctx)
-    (cond ((symbol? exp)
+    (cond ((symbol? exp) ;if symbol -> eval-symbol
            (eval-symbol exp))
-          ((!(list? exp))
+          ((!(list? exp)) ;if not a list (and not a symbol) -> return it
            exp)
-          ((list? exp)
-           
-           
-           
-           
-           ; ***************************************************************************************
-           ; *           The following lines should appear at the end, BELOW your code!            *
-           ; *                            Do NOT change the code below                             *
-           ; ***************************************************************************************
-           
-           ; Initially create system context
-           (define system-context (make-context system-definitions))
-           
-           ; Initially create user context
-           (define user-context (make-context user-definitions))
-           
-           
-           
-           ; *****************************************************************************************
-           ; * Use the following code to test your code. Copy it to the end of your interpreter code *
-           ; * file and run. If you see no error, all tests succeeded.                               *
-           ; * NOTE: These tests are not enough to make sure that your code is 100% correct. This is *
-           ; * only an infarstructure for you to create your own tests.                              *
-           ; *****************************************************************************************
-           
-           ; *** DO NOT LEAVE TEST CODE IN YOUR SUBMITTED FILE!!! ***
-           
-           ; A macro that is used to test the behavior of the interpreter
-           (defmacro assert (expr result)
-             `(if (not (equal? ,expr ,result))
-                  (error "Assertion failed: " (quote ,expr))))
-           
-           ; Basic tests
-           (assert (run-code 7) 7)
-           ; Primitive function execution and recursive evaluation
-           (assert (run-code '(+ 1 2)) 3)
-           (assert (run-code '(+ (- 2 1) 2)) 3)
-           (assert (run-code '(+ 1 (* 1 2))) 3)
-           ; Lambda execution test
-           (assert (run-code '((lambda (x y z) (+ x y z)) 1 2 3)) 6)
-           ; Bind test
-           (assert (run-code '((lambda (x . z) (cons x z)) 1 2 3)) '(1 2 3))
-           (assert (run-code '((lambda x x) 1 2 3)) '(1 2 3))
-           ; nLambda + eval test
-           (assert (run-code '(let ((a 1)) ((nlambda (x) (+ (eval x) 1)) a))) 2)
-           ; macro test
-           (assert (run-code '(let ((a 1)) ((macro (x) (list (quote +) x 1)) 1))) 2)
-           ; IF test
-           (assert (run-code '(let ((x 2)) (if (< x 3) 'small 'large))) 'small)
-           (assert (run-code '(let ((x 4)) (if (< x 3) 'small 'large))) 'large)
-           ; Bonus test
-           (if SWITCH_IP_ENABLED
-               (assert (run-code 
-                        '(let ((my-ip '(194 90 181 27)))
-                           (switch-ip my-ip
-                                      ((129 117) 'bezeq-international)
-                                      ((194 90) 'netvision)
-                                      ((85 44 2) 'zahav-012)
-                                      ((37 142 198) 'hot-net)
-                                      (default (display "Unknown IP address"))))) 'netvision))
+          (else ;Oh oh.....fun ends here.
+           (if (!(symbol? (car exp))) 
+               ;if the first elem is no symbol, we can exhale and try to smile;
+               ;if not --> we better run inside Molten Core screaming "LEEEROOOYYYY JENKINSSSSSS"
+               (exec-func (car exp) (cdr exp) ctx)
+               
+               
+               
+               
+               
+               
+               
+               ; ***************************************************************************************
+               ; *           The following lines should appear at the end, BELOW your code!            *
+               ; *                            Do NOT change the code below                             *
+               ; ***************************************************************************************
+               
+               ; Initially create system context
+               (define system-context (make-context system-definitions))
+               
+               ; Initially create user context
+               (define user-context (make-context user-definitions))
+               
+               
+               
+               ; *****************************************************************************************
+               ; * Use the following code to test your code. Copy it to the end of your interpreter code *
+               ; * file and run. If you see no error, all tests succeeded.                               *
+               ; * NOTE: These tests are not enough to make sure that your code is 100% correct. This is *
+               ; * only an infarstructure for you to create your own tests.                              *
+               ; *****************************************************************************************
+               
+               ; *** DO NOT LEAVE TEST CODE IN YOUR SUBMITTED FILE!!! ***
+               
+               ; A macro that is used to test the behavior of the interpreter
+               (defmacro assert (expr result)
+                 `(if (not (equal? ,expr ,result))
+                      (error "Assertion failed: " (quote ,expr))))
+               
+               ; Basic tests
+               (assert (run-code 7) 7)
+               ; Primitive function execution and recursive evaluation
+               (assert (run-code '(+ 1 2)) 3)
+               (assert (run-code '(+ (- 2 1) 2)) 3)
+               (assert (run-code '(+ 1 (* 1 2))) 3)
+               ; Lambda execution test
+               (assert (run-code '((lambda (x y z) (+ x y z)) 1 2 3)) 6)
+               ; Bind test
+               (assert (run-code '((lambda (x . z) (cons x z)) 1 2 3)) '(1 2 3))
+               (assert (run-code '((lambda x x) 1 2 3)) '(1 2 3))
+               ; nLambda + eval test
+               (assert (run-code '(let ((a 1)) ((nlambda (x) (+ (eval x) 1)) a))) 2)
+               ; macro test
+               (assert (run-code '(let ((a 1)) ((macro (x) (list (quote +) x 1)) 1))) 2)
+               ; IF test
+               (assert (run-code '(let ((x 2)) (if (< x 3) 'small 'large))) 'small)
+               (assert (run-code '(let ((x 4)) (if (< x 3) 'small 'large))) 'large)
+               ; Bonus test
+               (if SWITCH_IP_ENABLED
+                   (assert (run-code 
+                            '(let ((my-ip '(194 90 181 27)))
+                               (switch-ip my-ip
+                                          ((129 117) 'bezeq-international)
+                                          ((194 90) 'netvision)
+                                          ((85 44 2) 'zahav-012)
+                                          ((37 142 198) 'hot-net)
+                                          (default (display "Unknown IP address"))))) 'netvision))
