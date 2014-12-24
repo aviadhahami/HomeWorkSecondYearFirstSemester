bb927365f2ecc3213c779d87c30d41f99e8eb434
Aviad Hahami <aviadhahami@gmail.com>
2014-12-24 20:35:45 +0200

    trying to find a bug...WOOOHOO
    :bug:

--- Functional Prgrmng/Ex07/ex7-302188347.rkt
+++ Functional Prgrmng/Ex07/ex7-302188347.rkt
@@ -258,7 +258,7 @@
                     (exec-apply (driller currArgs 1) (driller currArgs 2) ctx))
                    (else 
                     (error "Unkown identifier: " funcPostEval))))))))
-                   
+(trace evaluate)
 
 
 
@@ -294,30 +294,30 @@
        (error "Assertion failed: " (quote ,expr))))
 
 ; Basic tests
-(assert (run-code 7) 7)
+;(assert (run-code 7) 7)
 ; Primitive function execution and recursive evaluation
-(assert (run-code '(+ 1 2)) 3)
-(assert (run-code '(+ (- 2 1) 2)) 3)
-(assert (run-code '(+ 1 (* 1 2))) 3)
+;(assert (run-code '(+ 1 2)) 3)
+;(assert (run-code '(+ (- 2 1) 2)) 3)
+;(assert (run-code '(+ 1 (* 1 2))) 3)
 ; Lambda execution test
-(assert (run-code '((lambda (x y z) (+ x y z)) 1 2 3)) 6)
+;(assert (run-code '((lambda (x y z) (+ x y z)) 1 2 3)) 6)
 ; Bind test
-(assert (run-code '((lambda (x . z) (cons x z)) 1 2 3)) '(1 2 3))
-(assert (run-code '((lambda x x) 1 2 3)) '(1 2 3))
+;(assert (run-code '((lambda (x . z) (cons x z)) 1 2 3)) '(1 2 3))
+;(assert (run-code '((lambda x x) 1 2 3)) '(1 2 3))
 ; nLambda + eval test
 (assert (run-code '(let ((a 1)) ((nlambda (x) (+ (eval x) 1)) a))) 2)
 ; macro test
-(assert (run-code '(let ((a 1)) ((macro (x) (list (quote +) x 1)) 1))) 2)
+;(assert (run-code '(let ((a 1)) ((macro (x) (list (quote +) x 1)) 1))) 2)
 ; IF test
-(assert (run-code '(let ((x 2)) (if (< x 3) 'small 'large))) 'small)
-(assert (run-code '(let ((x 4)) (if (< x 3) 'small 'large))) 'large)
+;(assert (run-code '(let ((x 2)) (if (< x 3) 'small 'large))) 'small)
+;(assert (run-code '(let ((x 4)) (if (< x 3) 'small 'large))) 'large)
 ; Bonus test
-(if SWITCH_IP_ENABLED
-    (assert (run-code 
-             '(let ((my-ip '(194 90 181 27)))
-                (switch-ip my-ip
-                           ((129 117) 'bezeq-international)
-                           ((194 90) 'netvision)
-                           ((85 44 2) 'zahav-012)
-                           ((37 142 198) 'hot-net)
-                           (default (display "Unknown IP address"))))) 'netvision))
+;(if SWITCH_IP_ENABLED
+;    (assert (run-code 
+;            '(let ((my-ip '(194 90 181 27)))
+;               (switch-ip my-ip
+;                          ((129 117) 'bezeq-international)
+;                          ((194 90) 'netvision)
+;                          ((85 44 2) 'zahav-012)
+;                          ((37 142 198) 'hot-net)
+;(default (display "Unknown IP address"))))) 'netvision))
