efaf0c16c7e668e9432505c7e1ec7739529b3207
Aviad Hahami <aviadhahami@gmail.com>
2015-01-10 16:52:21 +0200

    hope to god im fine with the runtimes...
    :godmode:

--- Functional Prgrmng/Ex08/ex08-302188347.rkt
+++ Functional Prgrmng/Ex08/ex08-302188347.rkt
@@ -37,11 +37,13 @@
       (sackFiller sortedItems '() capacity))))
 ;CS is just a pretty name and is helps us make things b-e-a-utiful!
 ;AAAAnd it also let us pick the comparison procedure we want (<,>,= etc..)
+;runtime -> O(nlogn) <-- [native]
 (define customSorter
   (λ (l op)
     (sort l op)))
 ;sackFiller gets the original list (OL) and the new list (NL) and the capacity (C)
 ;its fills the sack - matching the demands specified
+;runtime -> O(n)
 (define sackFiller
   (λ (ol nl c)
     (if (or (null? ol) (<= c 0))
@@ -52,16 +54,28 @@
 
 
 ;Part 3 - Backtracking
+;"!" func is used as syntatic sugar...so I feel like JS...#lame
+;runtime -> O(1)
 (define !
   (λ (arg)
     (not arg)))
-
+;getBiggerList recieves 2 lists and returns the bigger one...#MuchAwesome
+;runtime -> O(2n+1) == O(n)
 (define getBiggerList
   (λ (lstA lstB)
     (if (< (length lstA) (length lstB))
         lstB
         lstA)))
+;knapsack2 will most definately not win me the Turing award,
+;but it might gain me 100 pts for this assignment
+;the actual "knapsack2" is actualy a filter that provides one of two
+;calculating functions with relevant data.
 
+
+;runtime -> O(1 + O(knapsack2_by_weight) + O(knapsack2_by_val))
+;           O(1 + n + n)
+;           ============
+;            O(n)
 (define knapsack2 
   (λ (items capacity optimization-type)
     (cond ((eq? optimization-type 'WEIGHT);opt. type is by weight
@@ -69,6 +83,9 @@
           (else ;opt. type is by value
            (knapsack2_by_val items '() capacity)))))
 
+
+
+;runtime -> O(1 + 1 + n + n) ~ O(n)
 (define knapsack2_by_weight
   (λ (ol nl c)
     (if (or (null? ol) (<= c 0))
@@ -77,6 +94,7 @@
             (knapsack2_by_weight (cdr ol) nl c);not valid solution->we take only without
             (getBiggerList (knapsack2_by_weight (cdr ol) (append nl (list (car ol))) (- c (caar ol))) (knapsack2_by_weight (cdr ol) nl c))))));valid solution -> we take the one with both elements between the 2 solutions
 
+;runtime -> O(2 + n + n) ~ O(n)
 (define knapsack2_by_val
   (λ (ol nl c)
     (if (or (null? ol) (<= c 0))
