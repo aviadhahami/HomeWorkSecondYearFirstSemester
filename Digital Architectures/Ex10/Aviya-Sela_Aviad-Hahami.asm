a40826e421ff3fd320ad9bdb4fa0766aca4a0aed
Aviad Hahami <aviadhahami@gmail.com>
2015-01-13 08:43:24 +0200

    some progress

--- Digital Architectures/Ex10/Aviya-Sela_Aviad-Hahami.asm
+++ Digital Architectures/Ex10/Aviya-Sela_Aviad-Hahami.asm
@@ -1,31 +1,33 @@
-#DATA
+#first = 0
+#last = size -1
+#while (last - first > 1) {
+	#mid = (last-first)/2 + first
+	#if A[mid] == val
+		#break;
+	#if A[mid] > val
+		#last = mid;
+		#continue
+	#else
+		#first = mid;
+		#continue;
+#}
 .data 0x10000000
-arrSize: 	.word 10  # array in size 10
+.data 0x10000100 
+arr: 			.word 1,5,7,9,0xb,0xd,0x10,0x4000,0x50000,0x700000#ARRAY VALUES AS GIVEN IN THE ASSIGNMENT
+arrSize: 		.word 0x000a  # array in size 10
 toSearch: 	.word 0xd # the value we search
 
-#ARRAY VALUES AS GIVEN IN THE ASSIGNMENT
-.data 0x10000100 
-arr: 	.word 1
-	.word 5
-	.word 7
-	.word 9
-	.word 0xb
-	.word 0xd
-	.word 0x10
-	.word 0x4000
-	.word 0x50000
-	.word 0x700000
-	
+.text	
 main:
 	#	vars setup
 	#==================================
 	#s0 will be size,
 	#s1 will be first
 	#s2 will be last
-	add $s0,$zero,$zero #init $s0 -> 0
+	
 	lui $s0,0x1000 #pointing to 0x1000000
-	lw $s1,0($s0) #$s1 = size
-	add $s0,$zero,$s1 #S0 is now size, for comfort purposes
+	lw $s0,0($t9) #$s1 = size <------ SHOULD CHECK
+	
 	add $s1,$zero,$zero #init s1->0, will be first
 	subi $s2,$s0,1 #S2 (last) = s0 (size) - 1
 	j loop
