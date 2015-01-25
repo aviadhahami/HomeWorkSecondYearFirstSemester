b4e269909724cc283d46e7a839fba67d7ac939a3
Aviad Hahami <aviadhahami@gmail.com>
2015-01-25 19:03:36 +0200

    added printer and free

--- Prgrming_Wrkshp/Ex06/Ex06/Q#2/StringList/StringList/StringList2.c
+++ Prgrming_Wrkshp/Ex06/Ex06/Q#2/StringList/StringList/StringList2.c
@@ -7,5 +7,21 @@ StringList initStringList() {
 	return NULL;
 }
 StringList insertStringByLength(char* string, StringList list){}
-void printStringList(StringList list){}
-void freeStringList(StringList list) {}
+
+
+void printStringList(StringList list){
+	int counter = 1;
+	for (StringList currElem = list; currElem != NULL; currElem->next = list->next) {
+		printf("%d. %s\n", counter, currElem->data);
+		counter++;
+	}
+}
+
+
+void freeStringList(StringList list) {
+	while (list->next != NULL){
+		free(list->data);
+		free(list);
+		list = list->next;
+	}
+}
