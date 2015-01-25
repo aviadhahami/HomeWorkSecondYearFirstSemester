52b283df9907edcd8c2e4dbe53c7be09860d0742
Aviad Hahami <aviadhahami@gmail.com>
2015-01-25 16:01:33 +0200

    Created a stub for header

---
+++ Prgrming_Wrkshp/Ex06/Ex06/Q#2/StringList/StringList/StringList.h
@@ -0,0 +1,22 @@
+typedef struct StringList
+{
+
+};
+
+//Inits a StringList type of variable
+StringList initStringList();
+
+//Input: a StringList and a string
+//Logic: The function inserts the new string after the first string with same length.
+//		 If non exit -> will inject it @ top of StringList.
+//Output : StringList post insertion, all strings are sorted by length.
+//Expections: Will throw back NULL if couldn't allocate memo
+StringList insertStringByLength(char* string, StringList list);
+
+//Input: StringList
+//Output: Prints all the strings by their length (long to short),
+//		  prefixed by their index number
+void printStringList(StringList list);
+
+//Free's the allocated memory !IMPORTANT
+void freeStringList(StringList list);
